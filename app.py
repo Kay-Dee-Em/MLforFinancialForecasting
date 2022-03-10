d69bf2dc46fd28569558a67073944bf66390dc62
Klaudia Maruszak <klaudia.maruszak@gmail.com>
2022-03-10 15:10:13 +0100

    update app.py
    Signed-off-by: Klaudia Maruszak <klaudia.maruszak@gmail.com>

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import ImageGenerator as IG
 
 if __name__ == '__main__':
 
-  ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt', imgs_dir_name='GAF_binary', cmap='binary')
+  ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt')
   ig.generate_images()
-
-  
-
  
-
