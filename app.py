bad819e24d82921262071a32909b25add20ca45f
Klaudia Maruszak <klaudia.maruszak@gmail.com>
2022-03-10 15:08:40 +0100

    remove imgs with white space 2
    Signed-off-by: Klaudia Maruszak <klaudia.maruszak@gmail.com>

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import ImageGenerator as IG
-#import Model as M
 
 if __name__ == '__main__':
 
-  ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt', imgs_dir_name='GAF_binary', cmap='binary')#, generate_only_df_data=True)
+  ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt', imgs_dir_name='GAF_binary', cmap='binary')
   ig.generate_images()
-  #model = M.Model(df_closing_prices = ig.df_closing_prices)
-  #model.create_and_evalute_model()
-
 
   
 
