b8df831271941018961dd5489a8836db8e48f1e4
Klaudia Maruszak <klaudia.maruszak@pbc.pl>
2022-04-06 23:26:47 +0200

    update and rename script to create_plots.py
    Signed-off-by: Klaudia Maruszak <klaudia.maruszak@pbc.pl>

--- create_plots.py
+++ create_plots.py
@@ -3,25 +3,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 """"
-Auxiliary script to create visualization of preprocessing the data (linear plot, polar plot) for both strategies for chosen days i.e.:
-LONG - 2021-12-31
-SHORT - 2021-11-09 
+Auxiliary script to:
+- create visualization of preprocessing the data (linear plot, polar plot) for both strategies for chosen days i.e.:
+    LONG - 2021-12-31
+    SHORT - 2021-11-09
+
+- create visualization of cross-validation (CV) intervals for 9 neural networks 
+
 """
 
-if __name__ == '__main__':
+def strategy_visualization(df: pd.DataFrame()):
 
-    ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt', generate_only_df_data=True)
-    ig.generate_images()
-
-    plots_dir_name = 'PLOTS'
-    os.mkdir(plots_dir_name)
-
-    df_long = ig.df[(ig.df['DateTime'].dt.date.astype(str) >= '2021-12-03') & (ig.df['DateTime'].dt.date.astype(str) < '2022-01-01')]
-    df_long_next_day = ig.df[(ig.df['DateTime'].dt.date.astype(str) >= '2021-12-31') & (ig.df['DateTime'].dt.date.astype(str) < '2022-01-04')][-9:]
-    df_short = ig.df[(ig.df['DateTime'].dt.date.astype(str) >= '2021-10-13') & (ig.df['DateTime'].dt.date.astype(str) < '2021-11-10')]
-    df_short_next_day = ig.df[(ig.df['DateTime'].dt.date.astype(str) >= '2021-11-09') & (ig.df['DateTime'].dt.date.astype(str) < '2021-11-11')][-9:]
+    df_long = df[(df['DateTime'].dt.date.astype(str) >= '2021-12-03') & (df['DateTime'].dt.date.astype(str) < '2022-01-01')]
+    df_long_next_day = df[(df['DateTime'].dt.date.astype(str) >= '2021-12-31') & (df['DateTime'].dt.date.astype(str) < '2022-01-04')][-9:]
+    df_short = df[(df['DateTime'].dt.date.astype(str) >= '2021-10-13') & (df['DateTime'].dt.date.astype(str) < '2021-11-10')]
+    df_short_next_day = df[(df['DateTime'].dt.date.astype(str) >= '2021-11-09') & (df['DateTime'].dt.date.astype(str) < '2021-11-11')][-9:]
 
     dfs_strategy = [df_long, df_short]
     dfs_strategy_next_day = [df_long_next_day, df_short_next_day]
@@ -85,6 +85,66 @@ if __name__ == '__main__':
         fig.legend(fontsize=16, loc='lower left')
         fig.savefig(os.path.join(plots_dir_name, fig_names[number+2]), bbox_inches='tight', pad_inches=0, transparent=True)
         plt.close()
+
+
+def cross_validation_visualization(df: pd.DataFrame()):
+
+    plt.figure(figsize=(10,7))
+    plt.rc('axes', axisbelow=True)
+    x = df['DateTime']
+    y = df['Close']
+    plt.plot(x, y, color='#BFDCF7')
+    plt.fill_between(x, y, color='#BFDCF7')
+    plt.margins(0)
+
+    train_start = pd.to_datetime(dt.date.fromisoformat('2001-04-01'))
+    NN_number = 9
+    start_value = 15
+    for i in range(NN_number):
+
+        train_end = pd.to_datetime(train_start + relativedelta(months=13*12))
+        validation_end = pd.to_datetime(train_end + relativedelta(months=3.75*12))
+        test_end = pd.to_datetime(validation_end + relativedelta(months=2*12))
+
+        df_train_chunk = df.loc[df['DateTime'].between(train_start, train_end, inclusive='left')].reset_index(drop=True)
+        df_validation_chunk = df.loc[df['DateTime'].between(train_end, validation_end, inclusive='left')].reset_index(drop=True)
+        df_test_chunk = df.loc[df['DateTime'].between(validation_end, test_end, inclusive='left')].reset_index(drop=True)
+
+
+        x_train = df_train_chunk['DateTime']
+        y_train = [start_value] * len(x_train)
+        x_validation = df_validation_chunk['DateTime']
+        y_validation = [start_value] * len(x_validation)
+        x_test = df_test_chunk['DateTime']
+        y_test = [start_value] * len(x_test)
+
+        plt.plot(x_train, y_train, color='red', linewidth=2.5, linestyle='-')
+        plt.plot(x_validation, y_validation, color='orange', linewidth=2.5, linestyle='-')
+        plt.plot(x_test, y_test, color='green', linewidth=2.5)
+
+        train_start += relativedelta(months=3)
+        start_value += 30
+
+
+    plt.grid(True)
+    plt.savefig(os.path.join(plots_dir_name, 'cross_validation.png'), bbox_inches='tight', pad_inches=0, transparent=True)
+    plt.close()
+
+
+if __name__ == '__main__':
+
+    ig = IG.ImageGenerator(data_name = 'OIH_adjusted.txt', generate_only_df_data=True)
+    ig.generate_images()
+
+    plots_dir_name = 'PLOTS'
+    os.mkdir(plots_dir_name)
+
+    strategy_visualization(ig.df)
+    cross_validation_visualization(ig.df)
+
+
+
+    
 
 
         
